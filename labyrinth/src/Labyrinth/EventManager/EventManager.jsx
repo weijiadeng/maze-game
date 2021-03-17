a465cc3fef6b4283cd229af1e74fbac15908f463
wenhua <yao.wen@husky.neu.edu>
2021-03-17 13:36:20 -0700

    added buff and debuff event defination

--- labyrinth/src/Labyrinth/EventManager/EventManager.jsx
+++ labyrinth/src/Labyrinth/EventManager/EventManager.jsx
@@ -4,35 +4,114 @@ import { useSelector, useDispatch } from 'react-redux';
 import { pauseCount, readyCount } from '../elapseTimerSlice'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Modal from 'react-bootstrap/Modal'
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { resetBuffAndDebuff } from "../../reducers/playerStatusSlice";
+import { disableDarkMode, disableMiniMap, enableDarkMode, enableMiniMap, selectIsDark, selectShowMiniMap, selectSpeedModifier, speedDown, speedUp } from "../GameStatus/gameStatusSlice";
 
+const NUM_DEBUFF_TYPE = 3;
+const DARK_MODE_ID = 0;
+const SPEED_DOWN_ID = 1;
+const HIDE_MINI_MAP = 2;
+
+const NUM_BUFF_TYPE = 3;
+const BRIGHT_MODE_ID = 0;
+const SPEED_UP_ID = 1;
+const SHOW_MINI_MAP = 2;
 
 function StartEventRender() {
-    // Popup window currently not working
-    return (<div></div>);
+  // Popup window currently not working
+  return (<div></div>);
 }
 
 function startEventCallback(dispatch) {
-    alert("Game Start!");
-    dispatch(readyCount());
+  alert("Game Start!");
+  dispatch(readyCount());
 }
 
 function EndEventRender() {
-    return (<div></div>);
+  return (<div></div>);
 }
 
 function endEventCallback(dispatch) {
-    dispatch(pauseCount());
-    alert("Congrats! You've passed the game!");
+  dispatch(pauseCount());
+  alert("Congrats! You've passed the game!");
+}
+
+// With a Strong wind, all buff and debuff are cleared.
+function strongWindEventCallBack(dispatch) {
+  dispatch(resetBuffAndDebuff());
+}
+
+function strongWindEventRender() {
+  return (<div></div>);
 }
 
 
+// The smelly wind will add debuff: darkmode, minimap off, slowly move.
+function smellyWindEventCallBack(dispatch, select) {
+  // Generate a random debuff
+  const debuffID = Math.floor(Math.random() * NUM_DEBUFF_TYPE);
+  switch (debuffID) {
+    case DARK_MODE_ID:
+      const isDark = select(selectIsDark);
+      if (!isDark) {
+        dispatch(enableDarkMode());
+      }
+      break;
+    case SPEED_DOWN_ID:
+      dispatch(speedDown());
+      break;
+    case HIDE_MINI_MAP:
+      const isShowMiniMap = select(selectShowMiniMap);
+      if (isShowMiniMap) {
+        dispatch(disableMiniMap());
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+function smellyWindEventRender() {
+  return (<div></div>);
+}
+
+// With a Fresh wind, one buff is added: brightmode, minimap on, faster move.
+function freshWindEventCallBack(dispatch, select) {
+  // Generate a random buff
+  const buffID = Math.floor(Math.random() * NUM_BUFF_TYPE);
+  switch (buffID) {
+    case BRIGHT_MODE_ID:
+      const isDark = select(selectIsDark);
+      if (isDark) {
+        dispatch(disableDarkMode());
+      }
+      break;
+    case SPEED_UP_ID:
+      dispatch(speedUp());
+      break;
+    case SHOW_MINI_MAP:
+      const isShowMiniMap = select(selectShowMiniMap);
+      if (!isShowMiniMap) {
+        dispatch(enableMiniMap());
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+function freshWindEventRender() {
+  return (<div></div>);
+}
 
 function initEventMap(numX, numZ) {
-    const eventMap = Array(numX * numZ).fill(null);
-    eventMap[numX * (numZ - 1) + numX - 1] = [StartEventRender, startEventCallback];
-    eventMap[0] = [EndEventRender, endEventCallback];
-    return eventMap;
+  const eventMap = Array(numX * numZ).fill(null);
+  eventMap[numX * (numZ - 1) + numX - 1] = [StartEventRender, startEventCallback];
+  eventMap[0] = [EndEventRender, endEventCallback];
+  return eventMap;
 }
 
 
@@ -74,26 +153,27 @@ function Example() {
 
 
 export function EventManager({ discovered }) {
-    const numX = useSelector(selectNumX);
-    const numZ = useSelector(selectNumZ);
-    const [eventMap,] = useState(initEventMap(numX, numZ));
-    const posX = useSelector(selectPosX);
-    const posZ = useSelector(selectPosZ);
-    const currentIndex = posZ * numX + posX;
-    const currentAction = useSelector(selectAction);
-    const dispatch = useDispatch();
-    let currentCallback = () => { };
-    let toRender = () => { };
-    if (currentAction === NOTHING) {
-        if (!discovered.current[currentIndex]) {
-            if (eventMap[currentIndex] !== null) {
-                toRender = () => { eventMap[currentIndex][0]() };
-                currentCallback = () => { eventMap[currentIndex][1](dispatch) };
-                console.log("Triggered");
-            }
-        }
+  const numX = useSelector(selectNumX);
+  const numZ = useSelector(selectNumZ);
+  const [eventMap,] = useState(initEventMap(numX, numZ));
+  const posX = useSelector(selectPosX);
+  const posZ = useSelector(selectPosZ);
+  const currentIndex = posZ * numX + posX;
+  const currentAction = useSelector(selectAction);
+  const dispatch = useDispatch();
+  const select = useSelector;
+  let currentCallback = () => { };
+  let toRender;
+  if (currentAction === NOTHING) {
+    if (!discovered.current[currentIndex]) {
+      if (eventMap[currentIndex] !== null) {
+        toRender = () => { eventMap[currentIndex][0]() };
+        currentCallback = () => { eventMap[currentIndex][1](dispatch, select) };
+        console.log("Triggered");
+      }
     }
-    useEffect(currentCallback);
+  }
+  useEffect(currentCallback);
 
-    return <Example />;
+  return <Example />;
 }

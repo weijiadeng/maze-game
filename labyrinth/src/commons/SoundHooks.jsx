3dec239e2b3cad24fb1fb5560d28ca39316fbc08
wenhua <yao.wen@husky.neu.edu>
2021-03-19 16:28:24 -0700

    changed names: FreshAir to NeutralEffect.

--- labyrinth/src/commons/SoundHooks.jsx
+++ labyrinth/src/commons/SoundHooks.jsx
@@ -3,7 +3,7 @@ import useSound from 'use-sound';
 import GameCompletionSound from '../music/mixkit-game-level-completed-2059.wav'
 import PositiveEffectSound from '../music/mixkit-video-game-health-recharge-2837.wav'
 import NegativeEffectSound from '../music/mixkit-negative-guitar-tone-2324.wav'
-import FreshAirSound from '../music/mixkit-bonus-extra-in-a-video-game-2064.wav'
+import NeutralEffectSound from '../music/mixkit-bonus-extra-in-a-video-game-2064.wav'
 import ConfrontBattleSound from '../music/mixkit-arcade-retro-jump-223.wav'
 import GameOverSound from '../music/mixkit-player-losing-or-failing-2042.wav'
 
@@ -31,9 +31,9 @@ export function useNegativeEffectSound() {
     return { play, stop };
 }
 
-// FreshAir clears all effects, no matter positive or negative.
-export function useFreshAirSound() {
-    const [play, { stop }] = useSound(FreshAirSound, {
+// Clears all effects, no matter positive or negative.
+export function useNeutralEffectSound() {
+    const [play, { stop }] = useSound(NeutralEffectSound, {
         interrupt: true,
         volume: 0.65,
     });
@@ -59,7 +59,7 @@ const EffectSoundTestContainer = () => {
     const { play: playGameCompletionSound, stop: stopGameCompletionSound } = useGameCompletionSound();
     const { play: playPositiveEffectSound, stop: stopPositiveEffectSound } = usePositiveEffectSound();
     const { play: playNegativeEffectSound, stop: stopNegativeEffectSound } = useNegativeEffectSound();
-    const { play: playFreshAirSound, stop: stopFreshAirSound } = useFreshAirSound();
+    const { play: playNeutralEffectSound, stop: stopNeutralEffectSound } = useNeutralEffectSound();
     const { play: playConfrontBattleSound, stop: stopConfrontBattleSound } = useConfrontBattleSound();
     const { play: playGameOverSound, stop: stopGameOverSound } = useGameOverSound();
 
@@ -71,8 +71,8 @@ const EffectSoundTestContainer = () => {
             <button onClick={() => stopPositiveEffectSound()}>Stop PositiveEffectSound</button>
             <button onClick={() => playNegativeEffectSound()}>Play NegativeEffectSound</button>
             <button onClick={() => stopNegativeEffectSound()}>Stop NegativeEffectSound</button>
-            <button onClick={() => playFreshAirSound()}>Play FreshAirSound</button>
-            <button onClick={() => stopFreshAirSound()}>Stop FreshAirSound</button>
+            <button onClick={() => playNeutralEffectSound()}>Play NeutralEffectSound</button>
+            <button onClick={() => stopNeutralEffectSound()}>Stop NeutralEffectSound</button>
             <button onClick={() => playConfrontBattleSound()}>Play ConfrontBattleSound</button>
             <button onClick={() => stopConfrontBattleSound()}>Stop ConfrontBattleSound</button>
             <button onClick={() => playGameOverSound()}>Play GameOverSound</button>

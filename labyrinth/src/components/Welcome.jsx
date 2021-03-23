625ba9f33dd58a9b45d6ca7f03dc7170423a816a
weijiadeng <deng.weij@northeastern.edu>
2021-03-23 10:37:05 -0700

    Adjust fonts for welcome screen

--- labyrinth/src/components/Welcome.jsx
+++ labyrinth/src/components/Welcome.jsx
@@ -4,6 +4,14 @@ import styles from "./welcome.module.css"
 import { displayGame, displayModeSelection, displayWelcome, displayGuide } from '../reducers/programWindowSlice';
 import { CUSTOM, EASY, HARD, MEDIUM, setGameMode } from "../reducers/gameModeSlice";
 
+function WelcomeScreens(props) {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Maze</h1>
+            {props.children}
+        </div>);
+}
+
 export function ModeSelection() {
     const dispatch = useDispatch();
 
@@ -17,17 +25,17 @@ export function ModeSelection() {
     }
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Maze</h1>
+        <WelcomeScreens>
             <div className={styles.options}>
                 <h3 className={styles.buttons} onClick={() => handleModeSelect(EASY)}>Easy</h3>
                 <h3 className={styles.buttons} onClick={() => handleModeSelect(MEDIUM)}>Medium</h3>
                 <h3 className={styles.buttons} onClick={() => handleModeSelect(HARD)}>Hard</h3>
                 <h3 className={styles.buttons} onClick={() => handleModeSelect(CUSTOM)}>Custom</h3>
                 <h3 className={styles.buttons} onClick={() => handleGoBack()}>Go Back</h3>
-                </div>
+            </div>
             <div className={styles.footnote}></div>
-        </div>);
+        </WelcomeScreens>
+    );
 }
 
 export function Welcome() {
@@ -42,13 +50,12 @@ export function Welcome() {
     }
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Maze</h1>
+        <WelcomeScreens>
+
             <div className={styles.options}>
                 <h3 className={styles.buttons} onClick={() => handleGameStart()}>Game Start</h3>
                 <h3 className={styles.buttons} onClick={() => handleGuide()}>Guide</h3>
                 <h3 className={styles.buttons}>About</h3>
             </div>
-            <div className={styles.footnote}></div>
-        </div>);
+        </ WelcomeScreens>);
 }
